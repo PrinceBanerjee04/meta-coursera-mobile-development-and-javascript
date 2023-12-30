5cc727d5c680173e7ce700c6e7bf64b09ffc52f6
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-30 11:47:35 +0530

    Create oop.js

--- Object Oriented Programming/oop.js
+++ Object Oriented Programming/oop.js
@@ -18,3 +18,17 @@ class Person {
         }
     }
 }
+
+// Task 2: Code a Worker class
+class Worker extends Person {
+    constructor(name = "Tom", age = 20, energy = 100, xp = 0, hourlyWage = 10) {
+        super(name, age, energy);
+        this.xp = xp;
+        this.hourlyWage = hourlyWage;
+    }
+
+    goToWork() {
+        this.xp += 10;
+        this.energy -= 10; // Assuming working reduces energy
+    }
+}
