cb7e85d30f09c96f04786c0e12abce814f777cc3
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-31 11:26:22 +0530

    Create polymorphism.js

---
+++ Object Oriented Programming/polymorphism.js
@@ -0,0 +1,44 @@
+//POLYMORPHISM
+class Train {
+    constructor(color, lightsOn) {
+        this.color = color;
+        this.lightsOn = lightsOn;
+    }
+    toggleLights() {
+        this.lightsOn = !this.lightsOn;
+    }
+    lightsStatus() {
+        console.log('Lights on?', this.lightsOn);
+    }
+    getSelf() {
+        console.log(this);
+    }
+    getPrototype() {
+        var proto = Object.getPrototypeOf(this);
+        console.log(proto);
+    }
+}
+
+class HighSpeedTrain extends Train {
+    constructor(passengers, highSpeedOn, color, lightsOn) {
+        super(color, lightsOn);
+        this.passengers = passengers;
+        this.highSpeedOn = highSpeedOn;
+    }
+
+    toggleHighSpeed() {
+        this.highSpeedOn = !this.highSpeedOn;
+        console.log('High speed status:', this.highSpeedOn);
+    }
+
+	toggleLights() {
+
+        super.toggleLights();
+        super.lightsStatus();
+        console.log('Lights are 100% operational.');
+    }
+}
+
+let speedy = new HighSpeedTrain();
+
+speedy.toggleLights();
