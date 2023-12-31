cd1fbaae74b96e7dd4350bc92cf9476f4b3c19d6
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-31 11:17:49 +0530

    Create advanced.js

---
+++ Array and Object Iteration/advanced.js
@@ -0,0 +1,10 @@
+// Task 1
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake'];
+
+function logDairy() {
+  for (const product of dairy) {
+    console.log(product);
+  }
+}
+
+logDairy();
