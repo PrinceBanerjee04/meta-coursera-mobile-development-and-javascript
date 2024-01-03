d4216acff4f08078444d5c8e4b898e51feb9b347
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-01-03 15:41:36 +0530

    Update conditional_statements.js

--- conditional_statements.js
+++ conditional_statements.js
@@ -21,4 +21,35 @@ if (age >= 65) {
 
 
 // EXERCISE 2
+var day = `Sunday`;
 
+switch(day) {
+
+   case 'Monday':
+       console.log('Read a book');
+       break;
+
+   case 'Tuesday':
+       console.log('Watch a movie');
+       break;
+
+   case 'Wednesday':
+       console.log('Read a book');
+       break;
+
+   case 'Thursday':
+       console.log('Play basketball');
+       break;
+
+   case 'Friday':
+       console.log('Socialize');
+       break;
+
+   case 'Saturday':
+       console.log('Chill');
+       break;
+
+   case 'Sunday':
+       console.log('Have barbecue');
+       break;
+}
